3ce2bc45e549ab006ed06c593748933d96832c8a
Nikheel <bhangalenikheel@gmail.com>
2021-02-02 01:01:19 +0530

    IST commit done

---
+++ src/com/testng/test/Newcode3216.java
@@ -0,0 +1,14 @@
+package com.testng.test;
+
+public class Newcode3216 
+
+{
+	public void newMethod()
+	{
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	}
+	
+}
