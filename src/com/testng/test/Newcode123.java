329a6acef75c711ce1e5fa729a332ccbcb66400f
Nikheel <bhangalenikheel@gmail.com>
2021-02-02 00:54:01 +0530

    EST ARchitect

---
+++ src/com/testng/test/Newcode123.java
@@ -0,0 +1,16 @@
+package com.testng.test;
+
+public class Newcode123
+
+{
+public void display()
+{
+
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("");
+}
+}
