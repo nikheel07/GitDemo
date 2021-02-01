ff58b467c271f498297d8dc5973c8daaf13def98
Nikheel <bhangalenikheel@gmail.com>
2021-02-02 01:06:59 +0530

    final commit

---
+++ src/com/testng/test/newcode1798.java
@@ -0,0 +1,13 @@
+package com.testng.test;
+
+public class newcode1798 
+
+{
+	public void newMethod()
+	{
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	System.out.println("Mobile Logout mmmm");
+	}
+}
